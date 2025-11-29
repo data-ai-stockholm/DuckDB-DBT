2d1aa25f0903bec582a2ab6721ccdb9de4aa6105
Dinindu Seneviratne <dininduviduneth@gmail.com>
2025-11-29 23:53:54 +0530

    Complete temp ingestion_service

--- ingestion/fetch_stations.py
+++ ingestion/fetch_stations.py
@@ -2,30 +2,46 @@
 
 import requests
 import duckdb
+import os
 
 conn = duckdb.connect()
 
-next_page = "https://api.weather.gov/stations"
+def fetch_station_groups(page_limit=10):
+    next_page = "https://api.weather.gov/stations"
 
-while True:
-    conn.execute(f"""
-        CREATE OR REPLACE TABLE weather_stations AS
-        SELECT
-            UNNEST(observationStations) AS station_url,
-            pagination.next AS next_page
-        FROM read_json_auto('{next_page}');
-    """)
+    if not os.path.exists('ingestion_data/stations/'):
+        os.makedirs('ingestion_data/stations/')
 
-    next_page = conn.execute("""
-        SELECT
-            DISTINCT next_page
-        FROM weather_stations
-    """).fetchone()[0]
+    for _ in range(10):  # Limit to 10 pages for demo --> 5000 stations
+        conn.execute(f"""
+            CREATE OR REPLACE TABLE weather_stations AS
+            
+            SELECT
+                UNNEST(features)->>'properties'->>'stationIdentifier' AS station_id,
+                UNNEST(features)->>'id' AS station_url,
+                UNNEST(features)->>'geometry'->>'coordinates' AS coordinates,
+                UNNEST(features)->>'properties'->>'elevation'->>'value' AS elevation_m,
+                UNNEST(features)->>'properties'->>'name' AS station_name,
+                UNNEST(features)->>'properties'->>'timeZone' AS time_zone,
+                UNNEST(features)->>'properties'->>'forecast' AS forecast_url,
+                UNNEST(features)->>'properties'->>'county' AS county,
+                pagination.next AS next_page
+            FROM read_json_auto('{next_page}')
+        """)
 
-    cursor = next_page.split("cursor=")[-1]
+        next_page = conn.execute("""
+            SELECT
+                DISTINCT next_page
+            FROM weather_stations
+        """).fetchone()[0]
 
-    conn.execute(f"""
-        COPY (FROM weather_stations) TO 'ingestion_data/stations/weather_stations_{cursor}.parquet' (FORMAT PARQUET)
-    """).df()
+        cursor = next_page.split("cursor=")[-1]
 
-    print(f"Saved Page: {next_page}")
+        conn.execute(f"""
+            COPY (FROM weather_stations) TO 'ingestion_data/stations/station_group_{cursor}.parquet' (FORMAT PARQUET)
+        """).df()
+
+        print(f"Saved Page: {next_page}")
+
+if __name__ == "__main__":
+    fetch_station_groups()
